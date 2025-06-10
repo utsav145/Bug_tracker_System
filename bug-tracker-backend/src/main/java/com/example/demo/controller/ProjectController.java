61b72d8a401aeb7b16707fb1ff39c47c19fa4711
utsav145 <142640368+utsav145@users.noreply.github.com>
2025-06-10 18:46:14 +0530

    Update ProjectController.java

--- bug-tracker-backend/src/main/java/com/example/demo/controller/ProjectController.java
+++ bug-tracker-backend/src/main/java/com/example/demo/controller/ProjectController.java
@@ -25,7 +25,6 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api/projects")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "https://bug-tracker-system.onrender.com")
 public class ProjectController {
 
     private final ProjectRepository projectRepository;
