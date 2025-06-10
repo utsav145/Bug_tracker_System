1526005e88cd5aeee13499ee568f0eef792761b2
utsav145 <142640368+utsav145@users.noreply.github.com>
2025-06-10 18:44:05 +0530

    Update AuthController.java

--- bug-tracker-backend/src/main/java/com/example/demo/controller/AuthController.java
+++ bug-tracker-backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-
 public class AuthController {
 	private final PasswordEncoder passwordEncoder;
     private final UserService userService;
