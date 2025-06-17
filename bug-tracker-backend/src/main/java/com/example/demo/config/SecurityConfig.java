ed56588bd57c35c984d590b7dc42889a4df35de8
utsav145 <142640368+utsav145@users.noreply.github.com>
2025-06-17 20:49:50 +0530

    Update SecurityConfig.java

--- bug-tracker-backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ bug-tracker-backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                                 .requestMatchers("/api/auth/login").permitAll()
                                        
-                                .requestMatchers("/api/auth/register").authenticated()
+                                .requestMatchers("/api/auth/register").hasRole("ADMIN")
                                        
                                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                                 // TESTER
